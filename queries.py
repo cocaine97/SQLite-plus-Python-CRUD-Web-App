ec7925822ee89df4ffc0ba48bd82fe6060d3414e
DFG <cocaine31197@gmail.com>
2017-08-09 17:24:29 +0530

    queries.py furnished

--- queries.py
+++ queries.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base,Restaurant,MenuItem,User
+from database_setup import Base, Restaurant, MenuItem, User
 import bleach
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-DBSC = sessionmaker(bind = engine)
+DBSC = sessionmaker(bind=engine)
 session = DBSC()
 
 
@@ -21,93 +21,118 @@ def res_data_p(res_id):
     data = session.query(Restaurant).filter_by(id=res_id).one()
     return data
 
+
 def res_delete(x):
-    session.query(Restaurant).filter_by(id = x).\
-    delete(synchronize_session='evaluate')
+    session.query(Restaurant).filter_by(id=x).\
+        delete(synchronize_session='evaluate')
     session.commit()
 
-def res_edit_name(x,new_name):
+
+def res_edit_name(x, new_name):
     new_name = bleach.clean(new_name)
     data = session.query(Restaurant).filter_by(id=x).one()
     data.name = str(new_name)
     session.commit()
 
+
 def res_name(x):
     data = session.query(Restaurant).filter_by(id=x).one()
     return data.name
 
-def res_add(x,owner):
+
+def res_add(x, owner):
     naam = str(x)
-    temp = Restaurant(name=naam,user_id=owner)
+    temp = Restaurant(name=naam, user_id=owner)
     session.add(temp)
     session.commit()
 
-def res_edit(id,naam):
+
+def res_edit(id, naam):
     naam = bleach.clean(naam)
     temp = session.query(Restaurant).filter_by(id=id).one()
     temp.name = naam
     session.commit()
 
+
 def res_owner_id(res_id):
     data = session.query(Restaurant).filter_by(id=res_id).one().user_id
     return data
 
+
 # -------- MenuItem Queries --------
+
+
 def item_data(x):
-    data = session.query(MenuItem).filter_by(restaurant_id = x)
+    data = session.query(MenuItem).filter_by(restaurant_id=x)
     return data
-def item_by_course(res_id,item_course):
+
+
+def item_by_course(res_id, item_course):
     item_course = str(item_course)
-    data = session.query(MenuItem).filter_by(restaurant_id=res_id,course=item_course)
+    data = session.query(MenuItem).filter_by(restaurant_id=res_id,
+                                             course=item_course)
     return data
 
-def item_data_p(r,m):
-    data = session.query(MenuItem).filter_by(restaurant_id = r,id=m).one()
+
+def item_data_p(r, m):
+    data = session.query(MenuItem).filter_by(restaurant_id=r, id=m).one()
     return data
 
-def item_add(res_id,item_name,item_price,item_desc,item_course,owner):
+
+def item_add(res_id, item_name, item_price, item_desc, item_course, owner):
     item_name = bleach.clean(item_name)
     item_price = bleach.clean(item_price)
     item_desc = bleach.clean(item_desc)
-    data = MenuItem(restaurant_id=res_id,name=item_name,price=item_price,description=item_desc,course=item_course,user_id=owner)
+    data = MenuItem(restaurant_id=res_id, name=item_name,
+                    price=item_price, description=item_desc,
+                    course=item_course, user_id=owner)
     session.add(data)
     session.commit()
 
 
-def item_edit(res_id,item_id,item_name,item_price,item_desc,item_course):
-    data = session.query(MenuItem).filter_by(restaurant_id = res_id,id=item_id).one()
+def item_edit(res_id, item_id, item_name, item_price, item_desc, item_course):
+    data = session.query(MenuItem).filter_by(restaurant_id=res_id,
+                                             id=item_id).one()
     data.name = bleach.clean(item_name)
     data.price = bleach.clean(item_price)
     data.description = bleach.clean(item_desc)
     data.course = item_course
     session.commit()
 
-def item_delete(res_id,item_id):
-    session.query(MenuItem).filter_by(restaurant_id = res_id,id=item_id).\
-    delete(synchronize_session='evaluate')
+
+def item_delete(res_id, item_id):
+    session.query(MenuItem).filter_by(restaurant_id=res_id, id=item_id).\
+        delete(synchronize_session='evaluate')
     session.commit()
 
-def item_owner_id(res_id,item_id):
-    data = session.query(MenuItem).filter_by(restaurant_id=res_id,id=item_id).one().user_id
+
+def item_owner_id(res_id, item_id):
+    data = session.query(MenuItem).filter_by(restaurant_id=res_id,
+                                             id=item_id).one().user_id
     return data
 
 # - -   -   -   User Queries -  -   -   -   -
-def user_add(name,email,picture):
-    data = User(name=name,email=email,picture=picture)
+
+
+def user_add(name, email, picture):
+    data = User(name=name, email=email, picture=picture)
     session.add(data)
     session.commit()
 
+
 def user_email(user_id):
     data = session.query(User).filter_by(id=user_id).one().email
-    return data;
+    return data
+
 
 def user_check(email):
     flag = session.query(User).filter_by(email=email).first()
-    if(flag == None):
+    if(flag is None):
         flag = 0
     else:
         flag = 1
     return flag
+
 
 def user_data(email):
     data = session.query(User).filter_by(email=email).one()
